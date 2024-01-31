1d1f0405737bc8636142ae8d7b7ae931ac4afee9
Philipp <philipp.eckhard.staudinger@gmail.com>
2024-01-31 15:42:06 +0100

    clean

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
+
 const port = 4000
+
 app.use(express.static("public"))
 app.listen(port,() => {
     console.log("APP LISTEN ON PORT",port)
