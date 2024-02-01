d01d03230e447aaf5722c20bfad975c9f8a2bcf4
stoppeltob <stoppelkamp05@gmail.com>
2024-02-01 12:22:31 +0100

    error message login

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,13 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+function validateForm() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
-        location.reload();
+    if (username === '' || password === '') {
+      // Show error message
+      document.getElementById('login-error-msg').style.display = 'block';
     } else {
-        loginErrorMsg.style.opacity = 1;
+      // Submit the form or perform other actions
+      document.getElementById('login-error-msg').style.display = 'none';
+      document.getElementById('login-form').submit();
     }
-})
+  }
