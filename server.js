374fcec9ff085cc1e8cba3de37f39af7b2b3acc9
Philipp <philipp.eckhard.staudinger@gmail.com>
2024-02-07 11:07:04 +0100

    .

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const sqlite3 = require('sqlite3').verbose();
-const app = express()
 const session = require('express-session');
 const bcrypt = require('bcrypt')
 const bodyParser = require('body-parser');
+
+const app = express()
 
 const port = 4000
 
