9ffb68ce3528e047889c56aefbcedd0004cfd685
stoppeltob <stoppelkamp05@gmail.com>
2024-03-07 18:58:42 +0100

    änderungen an title und javascript bei swiper

--- public/js/index.js
+++ public/js/index.js
@@ -11,16 +11,3 @@ document.querySelector(".toggle-btn").addEventListener("click", function () {
   toggleNavbar();
 });
 
-//swiper initialisieren
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
